e631c6da17a8517a00d707a21c968491a2ef752b
Wang-Jun-Chao <shining-glory@qq.com>
2015-09-29 22:24:31 +0800

    commit

--- 004-点击事件/src/com/itheima/clickevent/MainActivity.java
+++ 004-点击事件/src/com/itheima/clickevent/MainActivity.java
@@ -31,7 +31,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
         System.out.println("第二个按钮被点击");
     }
 
+    // View：系统会把触发这个事件的对象传递进来
     public void getScore(View v) {
-        System.out.println("上不去了哎哎");
+        // 通过对View对象进判断，用户就可以知道用户点击的到底是哪一个对象
+        switch (v.getId()) {
+            case R.id.king:
+                System.out.println("下辈子吧");
+                break;
+            case R.id.diamond:
+                System.out.println("凑合凑合");
+                break;
+            case R.id.master:
+                System.out.println("想想就好");
+                break;
+        }
+//        System.out.println("上不去了哎哎");
     }
 }
