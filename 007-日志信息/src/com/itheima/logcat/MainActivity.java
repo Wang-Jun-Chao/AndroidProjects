76ab3cc6357ebbee3e779a232b6827664a8e8d43
Wang-Jun-Chao <shining-glory@qq.com>
2015-09-30 09:59:06 +0800

    commit

--- 007-日志信息/src/com/itheima/logcat/MainActivity.java
+++ 007-日志信息/src/com/itheima/logcat/MainActivity.java
@@ -2,6 +2,7 @@ package com.itheima.logcat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +12,15 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+//        System.out.println("小志，世界级AD西。");
+//        System.out.println("好吃不过饺子");
+
+        // 错误级别信息
+        Log.e("上miss", "谁能赐我一shi");
+        // 警告级别信息
+        Log.w("上miss", "谁能赐我一shi");
+        Log.i("上miss", "谁能赐我一shi");
+        Log.d("上miss", "谁能赐我一shi");
+        Log.v("上miss", "谁能赐我一shi");
     }
 }
---
+++ 009-使用API在内部存储中读写文件/src/com/itheima/rwinrom2/MaiActivity.java
@@ -0,0 +1,90 @@
+package com.itheima.rwinrom2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.*;
+
+public class MaiActivity extends Activity {
+    private EditText et_name;
+    private EditText et_pass;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_pass = (EditText) findViewById(R.id.et_pass);
+
+        readAccount();
+    }
+
+    public void readAccount() {
+        File file = new File("/data/data/com.itheima.rwinrom/login.txt");
+
+        if (file.exists()) {
+            BufferedReader fr = null;
+            try {
+                fr = new BufferedReader(new FileReader(file));
+                // 读取txt文件里的用户名和密码
+                String line = fr.readLine();
+                String[] ss = line.split("##");
+
+                // 数据回显
+
+                et_name.setText(ss[0]);
+                et_pass.setText(ss[1]);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fr != null) {
+                    try {
+                        fr.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void login(View view) {
+
+        String name = et_name.getText().toString();
+        String pass = et_pass.getText().toString();
+
+        CheckBox cb = (CheckBox) findViewById(R.id.cb);
+        // 判断选框是否被勾选
+        if (cb.isChecked()) {
+            // 内部存储空间的路径：/data/data/com.itheima.rwinrom/login.txt
+            File file = new File("/data/data/com.itheima.rwinrom/login.txt");
+            FileWriter fos = null;
+            try {
+                fos = new FileWriter(file, false);
+                fos.write((name + "##" + pass));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+    }
+}
