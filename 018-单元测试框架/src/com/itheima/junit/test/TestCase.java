b3e808797d92f136930c49140ea474cad6662283
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-05 09:33:00 +0800

    commit

--- 018-单元测试框架/src/com/itheima/junit/test/TestCase.java
+++ 018-单元测试框架/src/com/itheima/junit/test/TestCase.java
@@ -1,6 +1,7 @@
 package com.itheima.junit.test;
 
 import android.test.AndroidTestCase;
+import com.itheima.junit.utils.Utils;
 
 /**
  * Author: 王俊超
@@ -9,4 +10,11 @@ import android.test.AndroidTestCase;
  * Declaration: All Rights Reserved !!!
  */
 public class TestCase extends AndroidTestCase {
+    public void test() {
+//        System.out.println("测试框架跑起来了！");
+
+        int result = Utils.add(3, 5);
+        // 断言：用来检测实际值与期望值是否一致
+        assertEquals(8, result);
+    }
 }
---
+++ 018-单元测试框架/src/com/itheima/junit/utils/Utils.java
@@ -0,0 +1,14 @@
+package com.itheima.junit.utils;
+
+/**
+ * Author: 王俊超
+ * Date: 2015-10-05
+ * Time: 09:20
+ * Declaration: All Rights Reserved !!!
+ */
+public class Utils {
+    public static int add(int i, int j) {
+        return i + j;
+    }
+
+}
