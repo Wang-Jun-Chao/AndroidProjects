237a0d52710120812ed320906ae1dd0d9866bffa
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-09 07:56:49 +0800

    commit

---
+++ 040-Activity启动模式/src/com/itheima/lauchmode/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.itheima.lauchmode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: 王俊超
+ * Date: 2015-10-09
+ * Time: 07:15
+ * Declaration: All Rights Reserved !!!
+ */
+public class SecondActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
