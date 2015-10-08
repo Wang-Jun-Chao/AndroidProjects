67651908777aca370340aad92c5a80ed964475fe
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-08 10:30:31 +0800

    commit

---
+++ 039-Acivity在跳转时携带数据/src/com/itheima/senddata/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.itheima.senddata;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: 王俊超
+ * Date: 2015-10-08
+ * Time: 10:25
+ * Declaration: All Rights Reserved !!!
+ */
+public class SecondActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
