3defd54a980e11cea3d694965eebe054225eea5b
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-09 22:03:27 +0800

    commit

---
+++ 043-IP拨号器/src/com/itheima/ipdialer/MainActivity.java
@@ -0,0 +1,19 @@
+package com.itheima.ipdialer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void click(View view) {
+    }
+}
