02cdf120a8ff28b6caf48a38fc3e0abadbb15703
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-10 20:04:52 +0800

    commit

--- 048-发送自定义广播/src/com/itheima/sendcustomer/MainActivity.java
+++ 048-发送自定义广播/src/com/itheima/sendcustomer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.itheima.sendcustomer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +13,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void click(View view) {
+         // 发送自定义广播
+        Intent intent = new Intent();
+        // 广播中的action也是自定义的
+        intent.setAction("com.itheima.sendcustomer");
+        sendBroadcast(intent);
     }
 }
