67516ea57c67923bda896f9c4e44c678340a2339
Wang-Jun-Chao <shining-glory@qq.com>
2015-10-29 08:26:39 +0800

    ,commit

---
+++ 082-Fragment/src/com/itheima/fragment/MainActivity.java
@@ -0,0 +1,25 @@
+package com.itheima.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void click1(View view) {
+    }
+
+    public void click2(View view) {
+    }
+
+    public void click3(View view) {
+    }
+}
